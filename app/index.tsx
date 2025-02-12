c03294753765fb8db43b04f90bd68c8941b95cee
Lalaina0904 <lalainap19.aps2b@gmail.com>
2025-02-12 21:03:39 +0300

    debug and rebulding app

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,38 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Alert } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+import { PermissionsAndroid } from 'react-native';
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    async function requestUserPermission() {
+        const authStatus = await messaging().requestPermission();
+        const enabled =
+            authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+            authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+        if (enabled) {
+            console.log('Authorization status:', authStatus);
+        }
+    }
+
+    const getToken = async () => {
+        const token = await messaging().getToken();
+        console.log('there is the token:', token);
+    };
+    useEffect(() => {
+        requestUserPermission();
+        getToken();
+    }, []);
+
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+            <Text>Edit app/index.tsx to edit this screen test.</Text>
+        </View>
+    );
 }
